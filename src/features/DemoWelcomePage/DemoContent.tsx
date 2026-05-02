35c792dea0d78d96b4f8399a29cda198e8a4d806
Marccoder12 <mariomark407@gmail.com>
2026-05-02 19:58:27 +0100

    democontent update

--- src/features/DemoWelcomePage/DemoContent.tsx
+++ src/features/DemoWelcomePage/DemoContent.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 const DemoContent = () => {
 return (<div className="w-full h-full flex flex-col items-center justify-center gap-y-30">
     <span className="text-2xl font-bold">Welcome to Finetask Demo</span>
-                <Link to="/auth/register" className="bg-blue-400 p-4 px-8 rounded-lg hover:bg-blue-500 cursor-pointer text-xl">Start</Link>
+                <Link to="/home/dashboard/finetasks" className="bg-blue-400 p-4 px-8 rounded-lg hover:bg-blue-500 cursor-pointer text-xl">Start</Link>
                 </div>);
 }
 
